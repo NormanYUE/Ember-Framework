d1fa0810946cbf9626ef1365466185ec74e1f50e
乐成 <ra3monstar@gmail.com>
2026-06-22 18:35:34 +0800

    fix: switch Editor to source-based distribution
    - Remove pre-compiled Ember.Editor.dll (caused ScriptableObject resolution errors)
    - Add Editor .cs source files - Unity compiles via .asmdef
    - Update Ember.dll with public debug APIs

---
+++ Editor/EmberDebugger.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+
+namespace Ember.Editor
+{
+    /// <summary>
+    /// Ember 调试工具的菜单入口。
+    /// </summary>
+    public static class EmberDebugger
+    {
+        [MenuItem("Window/Ember/Systems")]
+        public static void OpenSystemsWindow()
+        {
+            SystemsWindow.Open();
+        }
+
+        [MenuItem("Window/Ember/Entities")]
+        public static void OpenEntitiesWindow()
+        {
+            EntitiesWindow.Open();
+        }
+
+        [MenuItem("Window/Ember/Archetypes")]
+        public static void OpenArchetypesWindow()
+        {
+            ArchetypesWindow.Open();
+        }
+
+        [MenuItem("Window/Ember/Component Types")]
+        public static void OpenComponentTypesWindow()
+        {
+            ComponentTypesWindow.Open();
+        }
+    }
+}
